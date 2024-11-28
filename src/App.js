797681c898d8327e92376d82862bce540453a8c9
MandalapuSai <mandalapusai99@gmail.com>
2024-11-28 08:53:28 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Sai Mandalapu</h2>
-      <h2><span style={{color:'red'}}>Working On</span> Digisphere Tech</h2>
+      <h2><span style={{color:'red'}}> Presently Working On</span> Digisphere Tech</h2>
     </div>
   );
 }
