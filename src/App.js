fd37f02c2c26935f0690553487387538d5b3f17b
MandalapuSai <mandalapusai99@gmail.com>
2024-11-28 14:55:58 +0530

    hello world add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Sai Mandalapu</h2>
+      <h2>Sai Mandalapu Hello World!!!!!</h2>
       <h2><span style={{color:'red'}}> Presently Working On</span> Digisphere Tech Solutions</h2>
     </div>
   );
